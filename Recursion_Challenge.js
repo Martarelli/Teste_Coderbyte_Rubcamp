b7643dc513e1b0d53698d5d54226b5612c0a0c52
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-09-05 00:07:55 -0300

    challenge finalizado

--- Recursion_Challenge.js
+++ Recursion_Challenge.js
@@ -1,3 +1,13 @@
 // Recursion Challenge
 // Have the function RecursionChallenge(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer.
 
+function RecursionChallenge(num) { 
+
+    // code goes here  
+    if(num === 0){
+      return 1;
+    } else {
+      return num * RecursionChallenge(num - 1)
+    }
+  
+  }
