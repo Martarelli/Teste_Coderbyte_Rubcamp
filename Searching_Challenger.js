ab7eb4319b450cb3d92e8167a64054ea931da5e0
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-09-04 22:42:51 -0300

    codigo finalizado

--- Searching_Challenger.js
+++ Searching_Challenger.js
@@ -8,3 +8,27 @@
 
 // If the username is valid then your program should return the string true, otherwise return the string false.
 
+function SearchingChallenge(str) { 
+  
+    // code goes here  
+    let regex = /^\w+$/;
+    let regexUnderscore = /^[_]$/
+    if(str.length >= 4 && str.length <= 25){
+      if(isNaN(str[0])){
+        if(regex.test(str)){
+          if(!regexUnderscore.test(str[str.length - 1])){
+            return "true";
+          } else {
+            return "false"; 
+          }
+        } else {
+            return "false"; 
+          }
+      } else {
+            return "false"; 
+          }
+    } else {
+      return "false"; 
+    }
+  
+  }
