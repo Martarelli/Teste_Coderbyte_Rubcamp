1c169a211f349759e572bb82f163b7ee65447fc8
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-09-04 21:59:07 -0300

    correção no enunciado

--- Array_Challenger_2.js
+++ Array_Challenger_2.js
@@ -1,7 +1,7 @@
 // Array Challenge
 // Have the function ArrayChallenge(arr) take the array of numbers stored in arr and return the string true if any two numbers can be multiplied so that the answer is greater than double the sum of all the elements in the array. If not, return the string false. For example: if arr is [2, 5, 6, -6, 16, 2, 3, 6, 5, 3] then the sum of all these elements is 42 and doubling it is 84. There are two elements in the array, 16 * 6 = 96 and 96 is greater than 84, so your program should return the string true.
 
-1function ArrayChallenge(arr) { 
+function ArrayChallenge(arr) { 
 
     let somaNumeros = 0;
     let maioresNumeros = [0, 0]
