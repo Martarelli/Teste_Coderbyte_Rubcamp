43e9cd56553749e8ef1ae19a9cda678617a80016
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-09-04 22:59:39 -0300

    inicio Math challenger 3

---
+++ Math_Challenge_3.js
@@ -0,0 +1,2 @@
+// Math Challenge
+// Have the function MathChallenge(num) take the num parameter being passed and return the next number greater than num using the same digits. For example: if num is 123 return 132, if it's 12453 return 12534. If a number has no greater permutations, return -1 (ie. 999).
